aa56770d90363e418dbc796305d5d2537ffec100
Jim Cronan <jcronan@uw.edu>
2021-02-16 18:05:35 -0800

    modified launch-simulation.sh to successfully launch AWS instances on the UW AWS account and added a notes .txt file to describe what changes I made in order to successfully launch EC2.

--- aws-launcher/launch-simulation.sh
+++ aws-launcher/launch-simulation.sh
@@ -47,18 +47,25 @@ elif [ $2 == "airfire" ]
     SUBNET_ID="--subnet-id subnet-15284970"
     AWS_PROFILE="--profile airfire"
     IAM_INSTANCE_PROFILE="--iam-instance-profile Name=cronan-fdm-eglin-simulations"
-elif[ $2 == "jcronantest" ]
+elif [ $2 == "jcronantest" ]
   then
     AWS_KEY_NAME="--key-name eafb_2020"
-    SUBNET_ID="--subnet-id subnet-15284970"
-    AWS_PROFILE="--profile jcronantest"
-    IAM_INSTANCE_PROFILE="--iam-instance-profile Name=jcronan-wildfire-sim"
+    AWS_PROFILE="--profile default"
+    IAM_INSTANCE_PROFILE="--iam-instance-profile Name=jcronantest"
 fi
 line=2
 tail -n +2 $1 | while IFS=',' read count params; do
   if [ ! $dryrun ]; then
-    aws ec2 run-instances --image-id ami-0ff4cc0162300f6df $AWS_KEY_NAME --user-data file://$SIM_ID/user_data_$line.ud --instance-type r3.large $IAM_INSTANCE_PROFILE --count $count $SUBNET_ID $AWS_PROFILE --security-group-ids "sg-06cb1607a02875a8e"
+    aws ec2 run-instances --image-id ami-0ff4cc0162300f6df $AWS_KEY_NAME --user-data file://$SIM_ID/user_data_$line.ud --instance-type t2.micro $IAM_INSTANCE_PROFILE --count $count $SUBNET_ID $AWS_PROFILE --security-group-ids "sg-0ab8431212d35629c"
   fi
   ((line++))
 done
 #old (pre-2019) AMI: ami-06775690a658b0847
+#old (pre 2019) Security Group "sg-06cb1607a02875a8e"
+#Original Structure
+#elif [ $2 == "jcronantest" ]
+#  then
+#    AWS_KEY_NAME="--key-name eafb_2020"
+#    AWS_PROFILE="--profile jcronantest"
+#    IAM_INSTANCE_PROFILE="--iam-instance-profile Name=jcronan-wildfire-sim"
+#Instance Type needed for full simulation: r3.large
